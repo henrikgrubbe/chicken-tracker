6492ea81bbe0d5dcee58474dee5fcaf08eca42c3
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2023-01-23 20:37:25 +0100

    Style input tab

--- src/components/TabInput.vue
+++ src/components/TabInput.vue
@@ -1,17 +1,83 @@
 <template>
   <div class="row my-3">
-    <h1 class="display-4">Input</h1>
+    <div>
+      <h1 class="display-2">Input</h1>
+      <hr>
+    </div>
   </div>
+
+  <div class="row mb-3">
+    <form>
+      <div>
+        <Datepicker v-model="date" auto-apply :clearable="false"
+                    :teleport-center="true" :enable-time-picker="false"/>
+      </div>
+    </form>
+  </div>
+
+  <div class="row mb-5">
+    <h2>Æg</h2>
+    <form>
+      <div class="input-group input-group-lg mb-3 egg-input">
+        <button class="btn btn-outline-secondary" type="button"
+                @click="numEggs--" :disabled="numEggs === 1">
+          -
+        </button>
+        <input type="number" min="1" class="form-control text-center" v-model="numEggs">
+        <button class="btn btn-outline-secondary" type="button"
+                @click="numEggs++">
+          +
+        </button>
+      </div>
+      <div class="d-grid gap-2">
+        <button class="btn btn-primary" type="button">Tilføj</button>
+      </div>
+    </form>
+  </div>
+
+  <div class="row">
+    <h2>Køb og salg</h2>
+    <form>
+      <div class="form-floating mb-3">
+        <input type="text" class="form-control" id="description" placeholder="Beskrivelse">
+        <label for="description">Beskrivelse</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input type="number" min="0" class="form-control" id="amount" placeholder="kr.">
+        <label for="description">Beløb</label>
+      </div>
+      <div class="d-grid gap-2">
+        <button class="btn btn-primary" type="button">Tilføj</button>
+      </div>
+    </form>
+  </div>
+
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
+
+const now = new Date();
 
 export default defineComponent({
-  name: "TabInput"
+  name: "TabInput",
+  components: {Datepicker},
+  data() {
+    return {
+      date: now,
+      numEggs: 1 as number,
+    };
+  },
 });
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.egg-input {
+  button, input {
+    font-size: 1.75rem !important;
+    line-height: 1.75rem !important;
+  }
+}
 </style>
