10c3415fcb4da7f7b5654b1bb82c79dfcba7ea08
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2023-04-12 06:14:58 +0000

    Show profile when logged in

--- src/components/auth/AuthView.vue
+++ src/components/auth/AuthView.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import '@passageidentity/passage-elements/passage-login'
+import '@passageidentity/passage-elements/passage-profile';
 import {PassageUser} from '@passageidentity/passage-elements/passage-user';
 
 import {defineComponent} from "vue";
@@ -8,26 +9,43 @@ export default defineComponent({
   name: "AuthView",
   data() {
     return {
-      appId: import.meta.env.VITE_PASSAGE_APP_ID
+      appId: import.meta.env.VITE_PASSAGE_APP_ID,
+      signedIn: false as boolean,
     };
   },
   methods: {
-    async signOut() {
-      const user = new PassageUser()
+    async setSignedIn(): Promise<void> {
+      try {
+        const signedIn = await new PassageUser().authGuard();
+        this.signedIn = signedIn;
+      } catch (error) {
+        this.signedIn = false;
+      }
+    },
+    async signOut(): Promise<void> {
+      const user = new PassageUser();
       await user.signOut();
+      await this.setSignedIn();
     }
   },
   mounted() {
+    this.setSignedIn();
   }
 });
 </script>
 
 <template>
   <div class="authContainer">
-    <passage-login :app-id="appId"></passage-login>
+    <passage-login v-if="!signedIn" :app-id="appId" default-country-code="DK"/>
+
+    <div v-else>
+      <passage-profile :app-id="appId" default-country-code="DK" />
+      <div class="d-flex justify-content-center">
+          <button class="btn btn-outline-primary" @click="signOut">Log ud</button>
+      </div>
+    </div>
   </div>
 </template>
-
 
 <style scoped>
 </style>
